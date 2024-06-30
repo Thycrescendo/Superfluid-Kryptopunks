592c8aec041a62c21c8bba319098b9f6a471e97d
thycrescendo <dmicholn@gmail.com>
2024-06-30 17:11:07 +0100

    folder structuring

--- packages/providers/WalletProvider.tsx
+++
@@ -1,9 +0,0 @@
-import { UseWalletProvider } from 'use-wallet';
-
-interface Props {
-  children: React.ReactNode;
-}
-
-export default ({ children }: Props) => (
-  <UseWalletProvider>{children}</UseWalletProvider>
-);
