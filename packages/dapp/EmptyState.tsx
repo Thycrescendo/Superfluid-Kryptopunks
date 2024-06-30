b8879e88145684a3266aae82947e8ee2e39261e3
thycrescendo <dmicholn@gmail.com>
2024-06-30 17:13:42 +0100

    changing the position of the dapp

--- packages/dapp/EmptyState.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export default styled.div`
-  margin-top: 2em;
-  width: 100%;
-  text-align: center;
-`;
--- packages/dapp/FormHeading.tsx
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export default styled.img`
-  margin-bottom: 3em;
-  height: 20px;
-`;
